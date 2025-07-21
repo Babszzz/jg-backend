7acdd6e0fc1b1db0f83c1d3b9fdb0cfc68147c59
Babszz <naija4nath@gmail.com>
2025-07-21 12:36:43 +0100

    landing page done sort off

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,11 @@ import express from "express";
 import { config } from "./config";
 import routes from "./routes";
 import { connectDB } from "./config/db";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 connectDB();
